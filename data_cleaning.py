19fd0f09422ae6ac2c980a92562b6dfb6cb226ed
ETHNHUNT <vipinzz001@gmail.com>
2024-08-13 22:34:35 -0400

    "Added data cleaning script to load CSV file, apply title case to 'Address' column, and save updated data to new CSV file."

---
+++ data_cleaning.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+# Load the Excel file
+#file_path = 'D:/LMIA-Map-Project/Q1 2024.csv'
+df = pd.read_csv('Q1 2024.csv')
+
+# Delete the first row (index 0)
+#df = df.drop(index=0)
+
+# Delete the last 8 rows
+#df = df[:-8]  # Slicing the DataFrame to exclude the last 8 rows
+
+# Apply title case to only the part after the comma in column 'D'
+def apply_title_after_comma(text):
+    parts = text.split(',', 1)
+    if len(parts) > 1:
+        # Apply title case to the second part after the comma
+        parts[0] = parts[0].strip().title()
+        return ', '.join(parts)
+    else:
+        return text
+
+df['Address'] = df['Address'].apply(apply_title_after_comma)
+
+
+# Save the updated DataFrame to a new Excel file
+updated_file_path = 'D:/LMIA-Map-Project/updated_q1_2024.csv' 
+df.to_csv('updated_q1_2024.csv', index= False)
