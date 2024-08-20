b4bb22c5c76f6dc96cc45b98c9a01b2653bc3acf
ETHNHUNT <vipinzz001@gmail.com>
2024-08-20 18:46:21 -0400

    main

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,40 +1,33 @@
 import pandas as pd
 import re
+
 # Load the Excel file
-df = pd.read_csv('Q1 2024.csv')
+df = pd.read_excel('2022.xlsx')
+df2 = pd.read_excel('2023.xlsx')
+df3= pd.read_excel('2024Q1.xlsx')
 
-# Apply title case to only the part after the comma in column 'D'
-def apply_title_after_comma(text):
-    parts = text.split(',', 1)
-    if len(parts) > 1:
-        # Apply title case to the second part after the comma
-       parts[0] = parts[0].strip().title()
-       return ', '.join(parts)
-    else:
-       return text
-# Function to title-case words before NL
-def title_case_before_postal(text):
-    parts = re.split(r'(\s*NL\s+)', text, 1)  # Split around the ' NL ' but keep it in the result
-    if len(parts) > 1:
-        # Only title-case the first part
-        parts[0] = re.sub(r"(\b\w+(?:'\w+)?\b)", lambda match: match.group(0).capitalize(), parts[0])
-    return ''.join(parts)
-df['Address'] = df['Address'].apply(apply_title_after_comma)
-df['Address'] = df['Address'].apply(title_case_before_postal)
+# Function to apply title case before the comma, handling apostrophes correctly
+def apply_title_before_comma(text):
+    if isinstance(text, str):  # Check if the value is a string
+        parts = text.split(',', 1)  # Split the text at the first comma
+        if len(parts) > 1:
+            # Apply title case to the part before the comma
+            parts[0] = ' '.join([word.capitalize() if "'" not in word else word[0].capitalize() + word[1:].lower() for word in parts[0].strip().split()])
+            return ', '.join(parts)  # Join back the parts with a comma and space
+        else:
+            return text
+    return text  # Return the value as is if it's not a string
 
-#splitting occupation and NOC code
-def split_occupation(occupation):
-    if pd.isnull(occupation):  # Check for NaN entries
-        return pd.Series([None, None])
-    noc_code, occupation_desc = occupation.split('-', 1)
-    noc_code = noc_code.strip().zfill(4)
-    return pd.Series([noc_code.strip(), occupation_desc.strip()])
-
+df['Address'] = df['Address'].apply(apply_title_before_comma)
 df["Employer"] = df["Employer"].str.upper().str.title()
-df[['NOC Code', 'Occupation']] = df['Occupation'].apply(split_occupation)
+df2['Address'] = df2['Address'].apply(apply_title_before_comma)
+df2["Employer"] = df2["Employer"].str.upper().str.title()
+df3['Address'] = df3['Address'].apply(apply_title_before_comma)
+df3["Employer"] = df3["Employer"].str.upper().str.title()
 
 # Save the updated DataFrame to a new Excel file
-updated_file_path = 'D:/LMIA-Map-Project/updated_q1_2024.csv' 
-df.to_csv('updated_q1_2024.csv', index= False)
-print(df)
+df.to_excel('Final_2022.xlsx', index= False)
+df2.to_excel('Final_2023.xlsx', index=False)
+df3.to_excel('Final_2024Q1.xlsx', index=False)
+print(df3[['Address']].head(10))
 
